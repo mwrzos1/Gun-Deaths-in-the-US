c357a0bbd41a8fad0cdf96df144b27a69f7f9a8f
Marcin <mwrzos1@students.towson.edu>
2018-06-30 01:17:06 -0400

    Second

--- Basics.py
+++ Basics.py
@@ -1,7 +1,6 @@
 
 
-#import file and display the first 5 rows
-
+#import file and display first 5 rows
 
 import csv
 file=open("guns.csv", "r")
@@ -9,4 +8,9 @@ csvreader= csv.reader(file)
 data= list(csvreader)
 print(data[0:5])
 
+#remove header and print first 5 rows
 
+headers = data[0:1]
+data = data[1:]
+print(headers)
+print(data[0:5])  
