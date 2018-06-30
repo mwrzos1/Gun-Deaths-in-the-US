0e260eaac98ded56fd220a1840cb45d052f3d514
Marcin <mwrzos1@students.towson.edu>
2018-06-30 02:11:29 -0400

    Gun deaths per year

--- Basics.py
+++ Basics.py
@@ -14,3 +14,16 @@ headers = data[0:1]
 data = data[1:]
 print(headers)
 print(data[0:5])  
+
+#extract years from column 2 
+
+years = [row[1] for row in data]
+year_counts= {}
+
+# each time element is a key in dictionary add 1
+for element in years:
+    if element in year_counts:
+        year_counts[element] = year_counts[element] + 1
+    else:
+        year_counts[element] = 1
+print(year_counts)
